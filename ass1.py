e66a32e176b3900a0269577fbc398eac2b787bd5
Kajaldengta <kajaldengta111@gmail.com>
2018-09-28 15:02:26 +0530

    first commit

---
+++ ass1.py
@@ -0,0 +1,26 @@
+#Q.1- Print anything you want on screen.
+print('hello everyone! i am learning python')
+#Q.2- Join two strings using '+'. 
+k="hello"+ "world"
+#Q.3- Take the input of 3 variables x, y and z . Print their values on screen. 
+x=input("enter the 1st value ")
+y=input("enter the 2nd value ")
+z=input("enter the 3rd value ")
+#Q.4- Print “Let’s get started” on screen.
+print("\"lets get started\"")
+#Q.5- Print the following values using placeholders. 
+#s=”Acadview” 
+#course=”Python” 
+#fees=5000
+s="Acadview"
+course="Python"
+fees=5000
+print("%s %s %d"%(s,course,fees))
+#Q.6- Find the area of circle 
+#pi = 3.14 
+#Take radius as input from user 
+#Print the area of circle
+pi=3.14
+r=int(input("enter the value of radius"))
+area=pi*r*r
+print("area of circle",area)
